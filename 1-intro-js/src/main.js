4ac07474363b135b8703ebb22805b0a24cc0bf5d
ivanacebo <ivan.acebo.glez@gmail.com>
2024-09-21 16:16:43 +0200

    primera parte

--- 1-intro-js/src/main.js
+++ 1-intro-js/src/main.js
@@ -6,9 +6,21 @@
 // Promesa con funcion asyncrona donde podemos leer los datos de nuestra api con json y traerlos al fronted
 const findAllUsers = async() => {
     const response = await fetch('http://jsonplaceholder.typicode.com/users');
-    return await response.json();
+    const users = await response.json();
+
+    const ul = document.createElement('ul');
+
+    users.forEach (user => {
+        const li = document.createElement('li');
+        li.innerText = user.name;
+
+        ul.appendChild(li);
+        console.log(user.name);
+    });
+
+    document.getElementById('root').append(ul);
 }
 
-const users = await findAllUsers();
-console.log(users);
-console.log('Hola que tal');
+findAllUsers();
+// console.log(users);
+// console.log('Hola que tal');
