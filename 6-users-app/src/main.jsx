847c5f160d2db9bee3da999831d631fbaf8edda2
ivanacebo <ivan.acebo.glez@gmail.com>
2024-09-29 11:02:21 +0200

    formulario para login

--- 6-users-app/src/main.jsx
+++ 6-users-app/src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { UsersApp } from './UsersApp'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { UsersApp } from "./UsersApp";
+import { LoginPage } from "./auth/pages/LoginPAge";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <UsersApp />
-  </StrictMode>,
-)
+    <LoginPage />
+  </StrictMode>
+);
