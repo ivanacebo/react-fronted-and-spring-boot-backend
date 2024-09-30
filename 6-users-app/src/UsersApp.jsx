8e21ac04d0afb7c277c750ac71ba37f9e5c3b963
ivanacebo <ivan.acebo.glez@gmail.com>
2024-09-30 11:36:22 +0200

    terminamos aplicación y pasamos a definir las rutas

--- 6-users-app/src/UsersApp.jsx
+++ 6-users-app/src/UsersApp.jsx
@@ -1,36 +1,10 @@
-import { useReducer } from "react";
 import { LoginPage } from "./auth/pages/LoginPAge";
 import { UsersPage } from "./pages/UsersPage";
-import { loginReducer } from "./auth/reducers/loginReducer";
-import Swal from "sweetalert2";
 import { Navbar } from "./components/layout/Navbar";
-
-const initialLogin = JSON.parse(sessionStorage.getItem("login")) || {
-  isAuth: false,
-  user: undefined,
-};
+import { useAuth } from "./auth/hooks/useAuth";
 
 export const UsersApp = () => {
-  const [login, dispatch] = useReducer(loginReducer, initialLogin);
-
-  const handlerLogin = ({ username, password }) => {
-    if (username === "admin" && password === "admin123") {
-      const user = { username: "admin" };
-      dispatch({ type: "login", payload: user });
-      sessionStorage.setItem("login", JSON.stringify({ isAuth: true, user }));
-    } else {
-      Swal.fire(
-        "Error de validacion",
-        "Usuario o contraseña incorrectos",
-        "error"
-      );
-    }
-  };
-
-  const handlerLogout = () => {
-    dispatch({ type: "logout" });
-    sessionStorage.removeItem("login");
-  };
+  const { login, handlerLogin, handlerLogout } = useAuth();
 
   return (
     <>
