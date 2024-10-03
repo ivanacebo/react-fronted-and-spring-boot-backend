19a29f25e0360b914d985cb436880460041cec35
ivanacebo <ivan.acebo.glez@gmail.com>
2024-10-03 10:38:16 +0200

    Configurando JWT authentication filter en back

---
+++ spring/backend-userapp/src/main/java/com/ivan/backend/userapp/backend_userapp/auth/SpringSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.ivan.backend.userapp.backend_userapp.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http.authorizeHttpRequests()
+                .requestMatchers(HttpMethod.GET, "/users")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf(config -> config.disable())
+                .sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
